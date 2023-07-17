271201f29baa25640fd541fdceea1ba96f271a73
felixp8 <felp8484@gmail.con>
2023-07-17 21:24:35 +0000

    remove unnecessary code

--- src/stavisky_lab_to_nwb/stavisky/staviskysortinginterface.py
+++ src/stavisky_lab_to_nwb/stavisky/staviskysortinginterface.py
@@ -109,28 +109,3 @@ class StaviskySortingInterface(RedisStreamSortingInterface):
             The starting time for all temporal data in this interface.
         """
         self.set_aligned_timestamps(aligned_timestamps=self.get_timestamps() + aligned_starting_time)
-
-    def align_by_interpolation(self, unaligned_timestamps: np.ndarray, aligned_timestamps: np.ndarray) -> None:
-        """
-        Interpolate the timestamps of this interface using a mapping from some unaligned time basis to its aligned one.
-
-        Use this method if the unaligned timestamps of the data in this interface are not directly tracked by a primary
-        system, but are known to occur between timestamps that are tracked, then align the timestamps of this interface
-        by interpolating between the two.
-
-        An example could be a metronomic TTL pulse (e.g., every second) from a secondary data stream to the primary
-        timing system; if the time references of this interface are recorded within the relative time of the secondary
-        data stream, then their exact time in the primary system is inferred given the pulse times.
-
-        Must be in units seconds relative to the common 'session_start_time'.
-
-        Parameters
-        ----------
-        unaligned_timestamps : numpy.ndarray
-            The timestamps of the unaligned secondary time basis.
-        aligned_timestamps : numpy.ndarray
-            The timestamps aligned to the primary time basis.
-        """
-        self.set_aligned_timestamps(
-            aligned_timestamps=np.interp(x=self.get_timestamps(), xp=unaligned_timestamps, fp=aligned_timestamps)
-        )
